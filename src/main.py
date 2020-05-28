2296248fd8b3350c5b310c3db412435b2e5db629
Bhimesh Chauhan <bhimeshchauhan@esarcbot.local>
2020-05-28 18:33:12 -0400

    add to the readme

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/<name>')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('main.html', name=None)
